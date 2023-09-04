36d624cba96f39cd6b97a4c64aed170691ffc8b6
Venu <venu@zmater.com>
2023-09-04 10:31:46 +0530

    user is modified

--- src/main/java/com/zmater/userservice/entity/User.java
+++ src/main/java/com/zmater/userservice/entity/User.java
@@ -1,33 +1,59 @@
 package com.zmater.userservice.entity;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
+@Table(name = "users")
 public class User {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	@Column(name="phone_number",nullable = false,unique=true)
+	@NotBlank(message = "Phone Number cannot be Empty")
 	private String phoneNumber;
+
 	private String name;
+	
 	private String email;
+	
 	private String referralCode;
 	@CreationTimestamp
 	private Timestamp createdAt;
 	@UpdateTimestamp
 	private Timestamp updatedAt;
 	
-	User()
-	{
-		
+	@OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
+	@JsonManagedReference
+	private List<Vehicle> vehicle=new ArrayList<Vehicle>();
+	
+	public User(){ }
+
+	public List<Vehicle> getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(List<Vehicle> vehicle) {
+		this.vehicle = vehicle;
 	}
 
 	public int getId() {
@@ -90,9 +116,6 @@ public class User {
 	public String toString() {
 		return "User [id=" + id + ", phoneNumber=" + phoneNumber + ", name=" + name + ", email=" + email
 				+ ", referralCode=" + referralCode + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + "]";
-	}
-	
-	
-	
+	}	
 
 }
